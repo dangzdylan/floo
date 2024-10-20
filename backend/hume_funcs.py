d857e3020c277f33b041ba9747f1d77513c02ca8
slapblackjack <jstnhoangg@gmail.com>
2024-10-20 09:54:10 -0700

    added data storage for assessment

--- backend/hume_funcs.py
+++ backend/hume_funcs.py
@@ -13,7 +13,28 @@ HUME_API_KEY = os.getenv("HUME_API_KEY")
 def top_emotions(file):
     client = AsyncHumeClient(api_key=HUME_API_KEY)
 
-    job_id = await client.expression_measurement.batch.start_inference_job(
-        files=[file],
-        notify=True,
+    local_filepaths = [
+        open("uploaded_audio.wav", mode="rb")
+    ]
+
+    # Create configurations for each model you would like to use (blank = default)
+    face_config = Face()
+    # Create a Models object
+    models_chosen = Models(face=face_config)
+    
+    # Create a stringified object containing the configuration
+    stringified_configs = InferenceBaseRequest(models=models_chosen)
+    # Start an inference job and print the job_id
+    job_id = await client.expression_measurement.batch.start_inference_job_from_local_file(
+        json=stringified_configs, file=local_filepaths
     )
+    print(job_id)
+
+
+    job_predictions = client.expression_measurement.batch.get_job_predictions(
+        id=job_id
+    )
+
+    print(job_predictions)
+
+top_emotions("backend/uploaded_audio.wav")
