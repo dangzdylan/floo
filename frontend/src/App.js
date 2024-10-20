409ed753b4c8f3d4393d6ed335fdcfc744681342
Arnav Khinvasara <arnavkhinvasara1@gmail.com>
2024-10-19 22:25:33 -0700

    onboarding entry

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -1,9 +1,11 @@
 import logo from './logo.svg';
 import './App.css';
 import InterviewSelection from './components/InterviewSelection';
+import Onboarding from './components/Onboarding';
 function App() {
   return (
     <div className="App">
+      <Onboarding />
     </div>
   );
 }
